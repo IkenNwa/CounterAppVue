8d8c9bcb5490d8df3b3f273e117c97427328ee84
Ikechukwu Nwachukwu <104449541+IkenNwa@users.noreply.github.com>
2023-03-13 15:48:17 +0100

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -5,3 +5,14 @@
 <template>
     <img src="../../public/oops-404-error-with-a-broken-robot-animate.svg" alt="">
 </template>
+
+<style scoped>
+    img{
+        width: 50%;
+    }
+    @media (width < 770px) {
+        img{
+            width: 100%;
+        }
+    }
+</style>
